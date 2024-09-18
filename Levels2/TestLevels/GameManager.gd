7e125455c5088d1c0c21cfdf16f8b3d2b959b382
concerned-doggo <codeformetoo@proton.me>
2024-09-19 00:18:11 +0530

    add game manager

--- Levels2/TestLevels/GameManager.gd
+++ Levels2/TestLevels/GameManager.gd
@@ -2,12 +2,8 @@ extends Node2D
 
 @onready var bosslabel = $"../bosslabel"
 @export var isBossDead: bool  = false
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if isBossDead:
 		bosslabel.text = "Boss Slained!"
